4a01f1ed6a40470d8f085f9dcb867e76814b526c
Lucas Melo <lucasluandemelo@gmail.com>
2022-07-12 22:39:17 -0300

    Aula 12. Subject
    Aprender a definição de subject e a possibilidade de enxugar mais o código.

--- exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
+++ exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
@@ -1,25 +1,22 @@
 require 'calculator'
- 
+
+# ao inves de usar a classe posso, usar um texto e na linha 6, remover o described_class 
+# para inserir a classe Calculator.
 describe Calculator do
     context '#sum' do
-        it 'with positive numbers' do
-            calc = Calculator.new 
+        subject(:calc) {  described_class.new() }
+        it 'with positive numbers' do 
             result = calc.sum(5, 7)
-            
             expect(result).to eq(12)  
         end
         
         it 'with negative and positive numbers' do
-            calc = Calculator.new 
             result = calc.sum(-5, 7)
-            
             expect(result).to eq(2)  
         end
 
         it 'with negative numbers' do
-            calc = Calculator.new 
             result = calc.sum(-5, -8)
-            
             expect(result).to eq(-13)  
         end
     end
