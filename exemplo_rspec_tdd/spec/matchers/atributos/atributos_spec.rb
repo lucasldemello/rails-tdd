cdfb0640429018f8b8921c9d2936c7bf68f77add
Lucas Melo <lucasluandemelo@gmail.com>
2022-08-10 22:27:29 -0300

    Aula 20. Matchers de atributos de classes.

---
+++ exemplo_rspec_tdd/spec/matchers/atributos/atributos_spec.rb
@@ -0,0 +1,15 @@
+require "pessoa"
+
+describe 'Atributos' do
+    it 'have_attributes' do
+      pessoa = Pessoa.new()
+      pessoa.nome =  "Belchior"
+      pessoa.idade =  76
+      # breve explicacao sobre alias de matchers https://gist.github.com/JunichiIto/f603d3fbfcf99b914f86
+      expect(pessoa).to have_attributes(nome: "Belchior", idade: 76)
+      expect(pessoa).to have_attributes(nome: a_string_starting_with("B"), idade: a_value >= 70)
+
+      
+    end
+    
+end
