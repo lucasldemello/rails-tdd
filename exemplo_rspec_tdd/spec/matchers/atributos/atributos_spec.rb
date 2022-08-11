edfc4fba2d8206b325cda3373df24686b265097e
Lucas Melo <lucasluandemelo@gmail.com>
2022-08-10 22:41:08 -0300

    Aula 21. Matchers Predicados

--- exemplo_rspec_tdd/spec/matchers/atributos/atributos_spec.rb
+++ exemplo_rspec_tdd/spec/matchers/atributos/atributos_spec.rb
@@ -8,8 +8,6 @@ describe 'Atributos' do
       # breve explicacao sobre alias de matchers https://gist.github.com/JunichiIto/f603d3fbfcf99b914f86
       expect(pessoa).to have_attributes(nome: "Belchior", idade: 76)
       expect(pessoa).to have_attributes(nome: a_string_starting_with("B"), idade: a_value >= 70)
-
-      
     end
     
 end
