108b4d118b2d741e347679d7c3a9220e596cade0
Lucas Melo <lucasluandemelo@gmail.com>
2022-08-10 21:04:14 -0300

    Aula 16. Matchers de igualdade

---
+++ exemplo_rspec_tdd/spec/matchers/comparacao/comparacao_spec.rb
@@ -0,0 +1,32 @@
+describe 'Matchers de Comparação' do
+  it '#equal - Testa se é o mesmo objeto' do
+    #x, y = Array.new(2, "ruby")
+    x = "ruby"
+    y = "ruby"
+    expect(x).not_to equal(y)
+    expect(x).to equal(x)
+  end
+
+  it '#be - Testa se é o mesmo objeto' do
+    #x, y = Array.new(2, "ruby")
+    x = "ruby"
+    y = "ruby"
+    expect(x).not_to be(y)
+    expect(x).to be(x)
+  end
+
+  it '#eql - Testa se é o mesmo valor/conteudo' do
+    #x, y = Array.new(2, "ruby")
+    x = "ruby"
+    y = "ruby"
+    expect(x).to eql(y)
+  end
+
+  it '#eq - Testa se é o mesmo valor/conteudo' do
+    #x, y = Array.new(2, "ruby")
+    x = "ruby"
+    y = "ruby"
+    expect(x).to eq(y)
+  end
+
+end
