7e42b7937f0f990ead8b1ada87bfe17d3e517e2d
Lucas Melo <lucasluandemelo@gmail.com>
2022-08-10 23:55:07 -0300

    Aula 24. Matchers para array

---
+++ exemplo_rspec_tdd/spec/matchers/arrays/arrays_spec.rb
@@ -0,0 +1,14 @@
+describe Array.new([5,3,4]) do
+  it '#include' do 
+    expect(subject).to include(3)
+    expect(subject).to include(5,4)
+  end
+
+  it '#contain_exactly' do
+    expect(subject).to contain_exactly(3,4,5)
+  end
+
+  it '#match_array' do
+    expect(subject).to match_array([5,3,4])
+  end
+end
