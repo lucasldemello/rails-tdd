3be0fd262767ced20d314d7577d017e0ed456edb
Lucas Melo <lucasluandemelo@gmail.com>
2022-08-12 00:12:33 -0300

    Aula 31. Helper Methods (Arbitrários e de Módulo)

---
+++ exemplo_rspec_tdd/helpers/helper.rb
@@ -0,0 +1,5 @@
+module Helper
+    def fruta # helper method arbitrário.
+        %w(banana uva manga).sample
+    end
+end
--- exemplo_rspec_tdd/spec/composicao/composicao_spec.rb
+++ exemplo_rspec_tdd/spec/composicao/composicao_spec.rb
@@ -1,8 +1,14 @@
+# Incluir dentro do teste ou no arquivo spec_helper.
+# require_relative 'helper' 
+# RSpec.configure do |conf|
+#     conf.include Helper
+# end
+
 describe "Ruby on Rails" do
     it { is_expected.to start_with("Ruby").and end_with("Rails")}
     it { expect(fruta).to eq('banana').or eq('uva').or eq('manga')}
 
-    def fruta
-        %w(banana uva manga).sample
-    end
+    # def fruta # helper method arbitrário.
+    #     %w(banana uva manga).sample
+    # end
 end
