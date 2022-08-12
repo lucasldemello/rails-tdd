b226eb253e63d00b8562e6bb2d8210afec16eefb
Lucas Melo <lucasluandemelo@gmail.com>
2022-08-11 22:17:02 -0300

    Aula 26. One Line Matchers

--- exemplo_rspec_tdd/spec/matchers/ranges/ranges_spec.rb
+++ exemplo_rspec_tdd/spec/matchers/ranges/ranges_spec.rb
@@ -1,6 +1,11 @@
 describe (1..7), 'Ranges' do
   it '#cover' do
+    is_expected.to cover(2) 
     expect(subject).to cover(3,6)
     expect(subject).not_to cover(0, 8)
   end
+
+  it {is_expected.to cover(2)} 
+  it {is_expected.to cover(3,5)} 
+  it {is_expected.not_to cover(-1, 14)} 
 end
