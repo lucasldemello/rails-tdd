152f9fb753004227ca424f42bd1373d7eff6071d
Lucas Melo <lucasluandemelo@gmail.com>
2022-08-15 20:01:10 -0300

    Aula 34. Helper methods (let e let!)

---
+++ exemplo_rspec_tdd/spec/matchers/atributos/atributos_let_spec.rb
@@ -0,0 +1,26 @@
+require "pessoa"
+
+describe 'Atributos' do
+    # before(:each) do 
+    #   @pessoa = Pessoa.new()
+    # end
+
+    let(:pessoa) { Pessoa.new }
+
+    it 'have_attributes of Belchior' do
+      pessoa.nome =  "Belchior"
+      pessoa.idade =  76
+      # breve explicacao sobre alias de matchers https://gist.github.com/JunichiIto/f603d3fbfcf99b914f86
+      expect(pessoa).to have_attributes(nome: "Belchior", idade: 76)
+      expect(pessoa).to have_attributes(nome: a_string_starting_with("B"), idade: a_value >= 70)
+    end
+
+    it 'have_attributes of Alceu' do
+      pessoa.nome =  "Alceu"
+      pessoa.idade =  85
+      # breve explicacao sobre alias de matchers https://gist.github.com/JunichiIto/f603d3fbfcf99b914f86
+      expect(pessoa).to have_attributes(nome: "Alceu", idade: 85)
+      expect(pessoa).to have_attributes(nome: a_string_starting_with("A"), idade: a_value >= 70)
+    end
+
+end
