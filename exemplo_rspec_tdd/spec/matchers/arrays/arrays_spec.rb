1abac4b6ea1b74122d6a45350e4a482441fbb61e
Lucas Melo <lucasluandemelo@gmail.com>
2022-08-15 21:06:33 -0300

    Aula 37. Negativando matchers

--- exemplo_rspec_tdd/spec/matchers/arrays/arrays_spec.rb
+++ exemplo_rspec_tdd/spec/matchers/arrays/arrays_spec.rb
@@ -1,8 +1,15 @@
+RSpec::Matchers.define_negated_matcher :exclude, :include
+
 describe Array.new([5,3,4]) do
   it '#include' do 
     expect(subject).to include(3)
     expect(subject).to include(5,4)
   end
+
+  it "should exclude 4" do
+    expect(subject).to exclude(9)
+  end
+  
 
   it '#contain_exactly' do
     expect(subject).to contain_exactly(3,4,5)
--- exemplo_rspec_tdd/spec/matchers/be_within/be_within_spec.rb
+++ exemplo_rspec_tdd/spec/matchers/be_within/be_within_spec.rb
@@ -1,6 +1,12 @@
+RSpec::Matchers.define_negated_matcher :be_not_within, :be_within
+
 describe "be_within" do
     # Somente trabalha com delta para numeros com ponto flutuante.
     it { expect(12.5).to be_within(0.5).of(12)}
     it { expect(11.7).to be_within(0.5).of(12)}
+    it { expect(11.2).to be_not_within(0.5).of(12)}
     it { expect([11.6, 12.1, 12.4]).to all (be_within(0.5).of(12))}
 end
+
+# delta 0.5
+# 11.5, 11.6, 11.7, 11.8, 11.9, 12, 12.1, 12.2, 12.3, 12.4, 12.5
